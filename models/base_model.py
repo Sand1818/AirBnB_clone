b3df83b306fca1678ed71458a8445bc06808a54a
Sand1818 <sandiso.jele@gmail.com>
2024-02-10 13:47:56 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -6,24 +6,20 @@ import uuid
 from datetime import datetime
 import models
 
-class BaseModel:
+class BaseModel():
     """BaseModel class of the Airbnb"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialises BaseModel"""
-        if not kwargs:
-                from models import storage
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
-                storage.new(self)
-            else:
-                kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-                kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-                for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns str representaion of BaseModel instance"""
