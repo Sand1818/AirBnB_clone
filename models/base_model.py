d43ddbaebc59d41cef8f8690e76de49194e688c7
Sand1818 <sandiso.jele@gmail.com>
2024-02-10 13:17:54 -0800

    base model

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Defines the BaseModel class
+"""
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel:
+    """BaseModel class of the Airbnb"""
+    def __init__(self):
+        """Initialises BaseModel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns str representaion of BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates to the current datetime updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns dictionary of BaseModel of instance"""
+        sj_dict = self.__dict__.copy()
+        sj_dict['__class__'] = self.__class__.__name__
+        sj_dict['created_at'] = self.created_at.isoformat()
+        sj_dict['updated_at'] = self.updated_at.isoformat()
+        return sj_dict
