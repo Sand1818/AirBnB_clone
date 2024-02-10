35e7c0bf20b0603527d4e505ab7a6d9cae4727b8
Sand1818 <sandiso.jele@gmail.com>
2024-02-10 13:34:55 -0800

    abase model

--- models/base_model.py
+++ models/base_model.py
@@ -10,16 +10,20 @@ class BaseModel:
     """BaseModel class of the Airbnb"""
     def __init__(self):
         """Initialises BaseModel"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        if not kwargs:
+                from models import storage
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                storage.new(self)
+            else:
+                kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+                for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """Returns str representaion of BaseModel instance"""
